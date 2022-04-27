6ab8f506a5c22b2da9a29f541217d8d8af5ea6cf
HaiHell0 <hoangh1@nku.edu>
2022-04-27 07:08:50 -0400

    stuff

--- app.js
+++ app.js
@@ -181,6 +181,5 @@ app.delete('/files/:id', (req, res) => {
   });
 });
 
-const port = 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const PORT = process.env.PORT
+app.listen(PORT, ()=>{console.log("app is listening on port")});
